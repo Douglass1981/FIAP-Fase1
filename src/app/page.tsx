33d35b4b07a2a6b7bd496eb2ad57e9b270ed76fe
Clara <clararemotto@gmail.com>
2025-05-16 20:25:45 -0300

    refactor: rename tags and add CSS classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,128 @@
 "use client";
 
-import theme from "@/styles/theme";
-import { Checkbox, ThemeProvider } from "@mui/material";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
-import TextField from "@mui/material/TextField";
+import { Box, Button, Checkbox, Container } from "@mui/material";
 import Image from "next/image";
+import Link from "@mui/material/Link";
+import { Poppins } from "next/font/google";
 import { useState } from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
 
 export default function HOME() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Grid
-        container
-        spacing={8}
-        columns={16}
-        direction="row"
-        style={{ height: "100vh" }}
-        sx={{
-          justifyContent: "center",
-          alignItems: "stretch",
-          
-        }}
-      >
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <div style={{ justifyItems: "center", paddingTop: "250px" }}>
+    <>
+      <main className={`${styles.login} ${poppins.className}`}>
+        <Box className={styles.login__container}>
+          <Box className={styles.login__container__logo_area}>
             <Image src="/logo.png" width="72" height="72" alt="Logo" />
-             <div>Poup.ai</div>
-          </div>
-         
-          <div>
-            <div style={{ fontSize: "36px", fontWeight: "bold" }}>Acesse</div>
-            <div style={{ fontSize: "24px" }}>Com seu login e senha</div>
-            <div style={{ fontSize: "18px" }}>Email</div>
-          </div>
-          <div>
-            <TextField
-              id="email"
-              label="Digite seu E-mail"
-              variant="outlined"
-              fullWidth
-            />
-          </div>
-          <div style={{ fontSize: "18px" }}>Senha</div>
-          <div>
-            <TextField
-              id="senha"
-              label="Digite sua Senha"
-              variant="outlined"
-              fullWidth
-            />{" "}
-          </div>
+            <h1 className={styles.login__container__logo_area__brand}>
+              Poup.ai
+            </h1>
+          </Box>
 
-          <div>
-            <Checkbox style={{ fontSize: "18px" }} />
-            Lembrar a minha senha
-          </div>
+          <Box className={styles.login__container__form_area}>
+            <Box className={styles.login__container__form_area__text}>
+              <h2 className={styles.login__container__form_area__text__title}>
+                Acesse
+              </h2>
+              <h3
+                className={styles.login__container__form_area__text__subtitle}
+              >
+                Com seu login e senha
+              </h3>
+            </Box>
+            <Box className={styles.login__container__form_area__form}>
+              <TextField
+                id="name"
+                label="Nome"
+                placeholder="Digite seu nome"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="email"
+                label="Email"
+                placeholder="Digite seu email"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="senha"
+                label="Senha"
+                placeholder="Digite seu senha"
+                variant="outlined"
+                fullWidth
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleTogglePassword} edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Box className={styles.login__container__form_area__form__checkbox}>
+                <Checkbox />
+                <p
+                  className={styles.login__container__form_area__form__checkbox__text}
+                >
+                  Lembrar minha senha
+                </p>
+              </Box>
+              <Button
+                variant="contained"
+                fullWidth
+                className={`${styles.login__container__form_area__form__button} ${poppins.className}`}
+              >
+                Entrar
+              </Button>
+              <Box className={styles.login__container__form_area__form__redirect}>
+                <p
+                  className={styles.login__container__form_area__form__redirect__text}
+                >
+                  Ainda não tem uma conta?{" "}
+                  <Link
+                    className={
+                      styles.login__container__form_area__form__redirect__text__link
+                    }
+                    href="http://localhost:3000/cadastro"
+                  >
+                    Cadastre-se
+                  </Link>
+                </p>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
 
-          <Button variant="outlined" fullWidth>
-            Entrar
-          </Button>
-
-          <div
-            style={{ fontSize: "16px", textAlign: "center", padding: "10px" }}
-          >
-            Ainda não tem uma conta?
-            <Link href="http://localhost:3000/cadastro">Cadastre-se</Link>
-          </div>
-        </Grid>
-
-        <Grid
-          size={{ xs: 16, md: 8 }}
-          sx={{
-            backgroundColor: "#002952",
-            color: "#ffffff",
-            justifyItems: "center",
-          }}
-        >
-          <div style={{ justifyItems: "center", paddingTop: "200px" }}>
-            <Image
-              src="/logoBemVindo.png"
-              width={467}
-              height={448}
-              alt="Bem Vindo"
-            />
-          </div>
-          <div
-            style={{
-              justifyItems: "center",
-              fontSize: "60px",
-              fontWeight: "bold",
-            }}
-          >
-            Bem Vindo!
-          </div>
-          <div
-            style={{
-              justifyItems: "center",
-              fontSize: "24px",
-            }}
-          >
-            <h1>Descubra soluções financeiras inovadoras</h1>
-            <h1>e confiávies para garantir um futuro</h1>
-            <h1>próspero</h1>
-          </div>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+        <Box className={styles.login__side}>
+          <Image
+            src="/undraw_transfer-money.svg"
+            width={467}
+            height={448}
+            alt="Bem Vindo"
+          />
+          <h2 className={styles.login__side__title}>Seja bem-vindo!</h2>
+          <h3 className={styles.login__side__description}>
+            Descubra soluções financeiras inovadoras e confiáveis para garantir
+            um futuro próspero
+          </h3>
+        </Box>
+      </main>
+    </>
   );
 }
