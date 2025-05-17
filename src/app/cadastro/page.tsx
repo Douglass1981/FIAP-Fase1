f070222517895d0178e3c995ee6505f803b6affd
Clara <clararemotto@gmail.com>
2025-05-17 18:20:08 -0300

    refactor(signup): rename classes and update semantic tags for improved clarity

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import theme from "@/styles/theme";
-import {
-  Button,
-  Checkbox,
-  Grid,
-  Link,
-  ThemeProvider,
-  useMediaQuery,
-} from "@mui/material";
-
-import TextField from "@mui/material/TextField";
-import Image from "next/image";
+import {Box, Button, Checkbox, IconButton, InputAdornment, TextField, useMediaQuery} from "@mui/material";
 import { useState } from "react";
+import styles from "./signup.styles.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 export default function Cadastro() {
   const [name, setName] = useState("");
@@ -56,119 +50,71 @@ export default function Cadastro() {
     }
   };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Grid
-        container
-        spacing={8}
-        columns={16}
-        direction="row"
-        style= { isMobile ? {height: "83vh"} : {height: "100vh"}}
-        sx={{
-          justifyContent: "center",
-          alignItems: "stretch",
-          ...(isMobile && {
-            backgroundColor: "#002952",
-           
-            color: "#ffffff",
-            paddingTop: "10px",
-          }),
-        }}
-      >
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <div style={{ justifyItems: "center",
-             paddingTop: isMobile?  "200px" : "250px",
-             display:"flex" }}>
-            <Image
-              src="/logo.png"
-              width={isMobile ? 232 : 72}
-              height={isMobile ? 232 : 72}
-              alt="Logo"
-            />
-            <div
-              style={{
-                marginTop: "8px",
-                fontSize: isMobile ? "0" : "64px",
-                fontWeight:"bold",
-                display:"flex"
-              }}
-            >
-              Poup.ai
-            </div>{" "}
-           
-          </div>
-          <div
-            style={{
-              fontSize: isMobile ? "24px" : "36px",
-              fontWeight: "bold",
-              marginTop: "30px",
-              
-            }}
-          >
-            Acesse
-          </div>{" "}
-        
-          <div style={{ fontSize: isMobile ? "18px" : "24px" }}>
-            Com seus dados pessoais
-          </div>{" "}
-        
-          <div style={{ fontSize: "18px",
-                         marginTop: "20px",
-                         color: isMobile? "black" : "black", }}>Nome</div>
-          
-          <div>
-            <TextField
-              id="nome"
-              label="Digite seu Nome"
-              variant="outlined"
-              fullWidth
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div style={{ fontSize: "18px",
-                  color: isMobile? "black" : "black",
-           }}>Email</div>
-          <div>
-            <TextField
-              id="email"
-              label="Digite seu E-mail"
-              variant="outlined"
-              fullWidth
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div style={{ fontSize: "18px",
-          color: isMobile? "black" : "black",
+  const [showPassword, setShowPassword] = useState(false);
 
-           }}>Senha</div>
-          <div>
-            <TextField
-              id="senha"
-              label="Digite sua Senha"
-              variant="outlined"
-              fullWidth
-              type="password"
-              value={password}
+  const handleTogglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+
+  return (
+    <>
+      <main className={styles['signup']}>
+        <Box className={styles['signup__container']}>
+          <Box className={styles['signup__container__logo_area']}>
+            <Image src="/logo.png" width="72" height="100" alt="Logo" className="signup__container__logo_area__image" />
+            <h1 className={styles['signup__container__logo_area_brand']}>Poup.ai</h1>
+          </Box>
+
+          <Box className={styles['signup__container__form-area']}>
+<Box className={styles['signup__container__form-area__text']}>
+              <h2 className={styles['signup__container__form-area__text__title']}>
+                Acesse
+              </h2>
+              <h3 className={styles['signup__container__form-area__text__subtitle']}>
+                Com seu signup e senha
+              </h3>
+            </Box>
+            <Box className={styles['signup__container__form-area__form']}>
+              <TextField
+                id="name"
+                label="Nome"
+                placeholder="Digite seu nome"
+                variant="outlined"
+                fullWidth
+                value={name}
+              onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                id="email"
+                label="Email"
+                placeholder="Digite seu email"
+                variant="outlined"
+                fullWidth
+                value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                id="senha"
+                label="Senha"
+                placeholder="Digite seu senha"
+                variant="outlined"
+                fullWidth
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleTogglePassword} edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                value={password}
               onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div style={{ paddingTop: "10px" }}>
-            <Button variant="outlined" fullWidth onClick={handleCadastroUser}>
-              Cadastrar
-            </Button>
-          </div>
-          <div
-            style={{ fontSize: "16px",
-               textAlign: "center",
-                padding: "10px",
-              color: isMobile? "black" : "black" }}
-          >
-            Já possui uma conta?
-            <Link href="http://localhost:3000/">Login</Link>
-          </div>
-          {cadastroSucesso && (
+              />
+
+              {cadastroSucesso && (
             <p style={{ color: "green", textAlign: "center" }}>
               Cadastro realizado com sucesso!
             </p>
@@ -176,27 +122,43 @@ export default function Cadastro() {
           {erroCadastro && (
             <p style={{ color: "red", textAlign: "center" }}>{erroCadastro}</p>
           )}
-        </Grid>
-        <Grid
-          size={{ xs: 16, md: 8 }}
-          sx={{
-            backgroundColor: "#002952",
-            color: "#ffffff",
-            justifyContent: "center",
-            alignItems: "center",
-            ...(isMobile && { display: "none" }),
-          }}
-        >
-          <div style={{ justifyItems: "center", paddingTop: "250px" }}>
-            <Image
-              src="/logoCadastro.png"
-              width="556"
-              height="410"
-              alt="Info Cadastro"
-            />
-          </div>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+              
+              <Button
+                variant="contained"
+                fullWidth
+                className={styles['signup__container__form-area__form__button']}
+                onClick={handleCadastroUser}
+              >
+                Entrar
+              </Button>
+              <Box className={styles['signup__container__form-area__form__redirect']}>
+                <p className={styles['signup__container__form-area__form__redirect__text']}>
+                  Já possui uma conta?{" "}
+                  <Link
+                    className={styles['signup__container__form-area__form__redirect__text__link']}
+                    href="http://localhost:3000/"
+                  >
+                    signup
+                  </Link>
+                </p>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box className={styles['signup__container-side']}>
+          <Image
+            src="/undraw_crypto.svg"
+            width={467}
+            height={448}
+            alt="Bem Vindo"
+            className="signup__container-side__image"
+          />
+          <h3 className={styles['signup__container-side__description']}>
+            Controle total das suas finanças, a qualquer hora, em qualquer lugar.
+          </h3>
+        </Box>
+      </main>
+    </>
   );
 }
