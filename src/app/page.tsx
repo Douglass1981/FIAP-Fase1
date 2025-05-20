135028e351fb949bc000a579eb1e0c8556166448
Clara <clararemotto@gmail.com>
2025-05-20 19:00:06 -0300

    fix(initial): fix name class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,66 +5,68 @@ import Image from "next/image";
 export default function Initial() {
     return (
         <>
-            <nav className={styles['nav']}>
-                <Box className={styles['nav__logo_area']}>
-                    <Image src="/logo.png" width="100" height="100" alt="Logo" className="nav__logo_area__image" />
-                    <h1 className={styles['nav__logo_area__brand']}>
-                        Poup.ai
-                    </h1>
-                </Box>
-                <Box className={styles['nav__redirect_pages']}>
-                    <Link
-                        className={styles['nav__redirect_pages__link']}
-                        href="http://localhost:3000/about"
+            <Box className={styles['content']}>
+                <nav className={styles['content__nav']}>
+                    <Box className={styles['content__nav__logo_area']}>
+                        <Image src="/logo.png" width="232" height="232" alt="Logo" className="content__nav__logo_area__image" />
+                        <h1 className={styles['content__nav__logo_area__brand']}>
+                            Poup.ai
+                        </h1>
+                    </Box>
+                    <Box className={styles['content__nav__redirect_pages']}>
+                        <Link
+                            className={styles['content__nav__redirect_pages__link']}
+                            href="http://localhost:3000/about"
+                        >
+                            Sobre nós
+                        </Link>
+                        <Link
+                            className={styles['content__nav__redirect_pages__link']}
+                            href="http://localhost:3000/services"
+                        >
+                            Serviços
+                        </Link>
+                        <Link
+                            className={styles['content__nav__redirect_pages__link']}
+                            href="http://localhost:3000/contact"
+                        >
+                            Entre em contato
+                        </Link>
+                    </Box>
+                    <Button
+                        variant="contained"
+                        id={styles['button']}
                     >
-                        Sobre nós
-                    </Link>
-                    <Link
-                        className={styles['nav__redirect_pages__link']}
-                        href="http://localhost:3000/services"
-                    >
-                        Serviços
-                    </Link>
-                    <Link
-                        className={styles['nav__redirect_pages__link']}
-                        href="http://localhost:3000/contact"
-                    >
-                        Entre em contato
-                    </Link>
-                </Box>
-                <Button
-                    variant="contained"
-                    className={styles['nav__button']}
-                >
-                    Login
-                </Button>
-            </nav>
-            <main>
-                <Box className={styles['desktop']}>
-                    <Box className={styles['desktop__container']}>
-                        <Box className={styles['desktop__container__left']}>
-                            <h2 className={styles['desktop__container__left__title']}>Seu futuro começa com escolhas inteligentes.</h2>
-                            <p className={styles['desktop__container__left__text']}>Descubra um novo jeito de cuidar das suas finanças com soluções inteligentes, práticas e seguras.</p>
-                            <p className={styles['desktop__container__left__text']}>Transforme sua relação com o dinheiro e alcance seus objetivos com confiança.</p>
-                        </Box>
-                        <Box className={styles['desktop__container__rigth']}>
-                            <Image src="/initial-image.png" width="477" height="576" alt="Initial image">
+                        Login
+                    </Button>
+                </nav>
+                <main>
+                    <Box className={styles['content__desktop']}>
+                        <Box className={styles['content__desktop__container']}>
+                            <Box className={styles['content__desktop__container__left']}>
+                                <h2 className={styles['content__desktop__container__left__title']}>Seu futuro começa com escolhas inteligentes.</h2>
+                                <p className={styles['content__desktop__container__left__text']}>Descubra um novo jeito de cuidar das suas finanças com soluções inteligentes, práticas e seguras.</p>
+                                <p className={styles['content__desktop__container__left__text']}>Transforme sua relação com o dinheiro e alcance seus objetivos com confiança.</p>
+                            </Box>
+                            <Box className={styles['content__desktop__container__rigth']}>
+                                <Image src="/initial-image.png" width="477" height="576" alt="Initial image">
 
-                            </Image>
+                                </Image>
+                            </Box>
                         </Box>
                     </Box>
-                </Box>
-                <Box className={styles['mobile']}>
-                    <h2 className={styles['mobile__title']}>Bem Vindo!</h2>
-                    <p className={styles['mobile__text']}>Descubra soluções financeiras inovadoras e confiáveis para garantir um futuro próspero.</p>
-                    <Button variant="contained" fullWidth className={styles['mobile__button']}>
-                        Acessar
-                    </Button>
-                </Box>
-            </main>
-            <footer className={styles['footer']}>
-                <p>© 2025 Poup.ai | Todos os direitos reservados</p>
-            </footer>
+                    <Box className={styles['content__mobile']}>
+                        <h2 className={styles['content__mobile__title']}>Bem Vindo!</h2>
+                        <p className={styles['content__mobile__text']}>Descubra soluções financeiras inovadoras e confiáveis para garantir um futuro próspero.</p>
+                        <Button variant="contained" fullWidth className={styles['content__mobile__button']}>
+                            Acessar
+                        </Button>
+                    </Box>
+                </main>
+                <footer className={styles['content__footer']}>
+                    <p>© 2025 Poup.ai | Todos os direitos reservados</p>
+                </footer>
+            </Box>
         </>
     )
 }
