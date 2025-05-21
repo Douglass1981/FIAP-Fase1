8f0aa89ce5b270f78ca761c119070b712de7cd9b
Clara <clararemotto@gmail.com>
2025-05-21 16:00:42 -0300

    feat(about): add main structure

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -41,6 +41,7 @@ export default function Initial() {
             </Link>
           </Box>
           <Button
+            
             variant="contained"
             href="http://localhost:3000/login"
             className={styles["content__nav__button"]}
@@ -48,7 +49,42 @@ export default function Initial() {
             Login
           </Button>
         </nav>
-
+        <main>
+          <Box className={styles["container"]}>
+            <h2 className={styles["container__title"]}>
+              No Poup.ai, acreditamos que todos merecem ter controle sobre suas
+              finanças de forma simples e inteligente.
+            </h2>
+            <Box className={styles["container__material"]}></Box>
+            <Box className={styles["container__material__left"]}>
+              <p className={styles["container__material__left__text"]}>
+                Somos uma plataforma criada para ajudar você a entender,
+                planejar e otimizar seus gastos com tecnologia acessível e
+                insights personalizados.
+              </p>
+              <p className={styles["container__material__left__text"]}>
+                Acreditamos que educação financeira não precisa ser complicada.
+                Com o Poup.ai, você tem uma visão clara do que faz sentido
+                manter, ou melhorar — sem fórmulas mágicas, apenas
+                transparência, praticidade e resultados.
+              </p>
+              <p className={styles["container__material__left__text"]}>
+                Nosso compromisso é com a sua liberdade financeira. Estamos aqui
+                para mostrar que, com informação e organização, seu dinheiro
+                pode trabalhar melhor para você.
+              </p>
+            </Box>
+            <Box className={styles["container__material__right"]}>
+              <Image
+                src="/undraw_pay-online.svg"
+                width="72"
+                height="100"
+                alt="Logo"
+                style={{ width: "15%", height: "auto" }}
+              />
+            </Box>
+          </Box>
+        </main>
         <footer className={styles["content__footer"]}>
           <p>© 2025 Poup.ai | Todos os direitos reservados</p>
         </footer>
