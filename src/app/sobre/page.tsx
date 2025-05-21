8daecfcb55d2a697d4de46dfcc3623067e59d556
Clara <clararemotto@gmail.com>
2025-05-21 17:17:19 -0300

    feat(style): add container and footer style

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -62,33 +62,34 @@ export default function Initial() {
               No Poup.ai, acreditamos que todos merecem ter controle sobre suas
               finanças de forma simples e inteligente.
             </h2>
-            <Box className={styles["container__material"]}></Box>
-            <Box className={styles["container__material__left"]}>
-              <p className={styles["container__material__left__text"]}>
-                Somos uma plataforma criada para ajudar você a entender,
-                planejar e otimizar seus gastos com tecnologia acessível e
-                insights personalizados.
-              </p>
-              <p className={styles["container__material__left__text"]}>
-                Acreditamos que educação financeira não precisa ser complicada.
-                Com o Poup.ai, você tem uma visão clara do que faz sentido
-                manter, ou melhorar — sem fórmulas mágicas, apenas
-                transparência, praticidade e resultados.
-              </p>
-              <p className={styles["container__material__left__text"]}>
-                Nosso compromisso é com a sua liberdade financeira. Estamos aqui
-                para mostrar que, com informação e organização, seu dinheiro
-                pode trabalhar melhor para você.
-              </p>
-            </Box>
-            <Box className={styles["container__material__right"]}>
-              <Image
-                src="/undraw_pay-online.svg"
-                width="72"
-                height="100"
-                alt="Logo"
-                style={{ width: "15%", height: "auto" }}
-              />
+            <Box className={styles["container__material"]}>
+              <Box className={styles["container__material__left"]}>
+                <p className={styles["container__material__left__text"]}>
+                  Somos uma plataforma criada para ajudar você a entender,
+                  planejar e otimizar seus gastos com tecnologia acessível e
+                  insights personalizados.
+                </p>
+                <p className={styles["container__material__left__text"]}>
+                  Acreditamos que educação financeira não precisa ser
+                  complicada. Com o Poup.ai, você tem uma visão clara do que faz
+                  sentido manter, ou melhorar — sem fórmulas mágicas, apenas
+                  transparência, praticidade e resultados.
+                </p>
+                <p className={styles["container__material__left__text"]}>
+                  Nosso compromisso é com a sua liberdade financeira. Estamos
+                  aqui para mostrar que, com informação e organização, seu
+                  dinheiro pode trabalhar melhor para você.
+                </p>
+              </Box>
+              <Box className={styles["container__material__right"]}>
+                <Image
+                  src="/undraw_pay-online.svg"
+                  width="72"
+                  height="100"
+                  alt="Logo"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Box>
             </Box>
           </Box>
         </main>
