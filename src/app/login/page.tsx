feb3a3fe94494dbd2b91ba0a6862179d7d8de50f
Clara <clararemotto@gmail.com>
2025-05-23 07:20:49 -0300

    fix(login): adjust pull request errors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,53 +1,28 @@
 "use client";
-
-<<<<<<< HEAD
-import { Box, Button, Checkbox, Container } from "@mui/material";
-import Image from "next/image";
-import Link from "@mui/material/Link";
-import { Poppins } from "next/font/google";
-import { useState } from "react";
-import { TextField, InputAdornment, IconButton } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-// import styles from "./home.styles.module.scss";
-
-export default function HOME() {
-=======
-import { Box, Button, Checkbox} from "@mui/material";
+import { Box, Button, Checkbox } from "@mui/material";
 import Image from "next/image";
 import Link from "@mui/material/Link";
 import { useState } from "react";
 import { TextField, InputAdornment, IconButton } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-<<<<<<< HEAD:src/app/page.tsx
-import styles from "./home.styles.module.scss";
-import {colors} from "./mui.styles"
-=======
 import styles from "./login.styles.module.scss";
-import {colors} from "../mui.styles";
+import { colors } from "../mui.styles";
 import { useMediaQuery } from "@mui/material";
->>>>>>> dev:src/app/login/page.tsx
 
 export default function Login() {
->>>>>>> fa0f9d98927fa9d1bef3df5e214280aeca74b432
   const [showPassword, setShowPassword] = useState(false);
 
   const handleTogglePassword = () => {
     setShowPassword((prev) => !prev);
   };
 
-<<<<<<< HEAD
-=======
   const isMobile = useMediaQuery("(max-width:767px)");
 
->>>>>>> fa0f9d98927fa9d1bef3df5e214280aeca74b432
   return (
     <>
-      <main className={styles['login']}>
-        <Box className={styles['login__container']}>
-          <Box className={styles['login__container__logo_area']}>
-<<<<<<< HEAD
-            <Image src="/logo.png" width="100" height="100" alt="Logo" className="login__container__logo_area__image" />
-=======
+      <main className={styles["login"]}>
+        <Box className={styles["login__container"]}>
+          <Box className={styles["login__container__logo_area"]}>
             {isMobile ? (
               <Image
                 src="/logo.png"
@@ -65,22 +40,28 @@ export default function Login() {
                 style={{ width: "15%", height: "auto" }}
               />
             )}
->>>>>>> fa0f9d98927fa9d1bef3df5e214280aeca74b432
-            <h1 className={styles['login__container__logo_area__brand']}>
+
+            <h1 className={styles["login__container__logo_area__brand"]}>
               Poup.ai
             </h1>
           </Box>
 
-          <Box className={styles['login__container__form-area']}>
-            <Box className={styles['login__container__form-area__text']}>
-              <h2 className={styles['login__container__form-area__text__title']}>
+          <Box className={styles["login__container__form-area"]}>
+            <Box className={styles["login__container__form-area__text"]}>
+              <h2
+                className={styles["login__container__form-area__text__title"]}
+              >
                 Acesse
               </h2>
-              <h3 className={styles['login__container__form-area__text__subtitle']}>
+              <h3
+                className={
+                  styles["login__container__form-area__text__subtitle"]
+                }
+              >
                 Com seu login e senha
               </h3>
             </Box>
-            <Box className={styles['login__container__form-area__form']}>
+            <Box className={styles["login__container__form-area__form"]}>
               <TextField
                 id="name"
                 label="Nome"
@@ -112,30 +93,48 @@ export default function Login() {
                   ),
                 }}
               />
-              <Box className={styles['login__container__form-area__form__checkbox']}>
+              <Box
+                className={
+                  styles["login__container__form-area__form__checkbox"]
+                }
+              >
                 <Checkbox />
-                <p className={styles['login__container__form-area__form__checkbox__text']}>
+                <p
+                  className={
+                    styles["login__container__form-area__form__checkbox__text"]
+                  }
+                >
                   Lembrar minha senha
                 </p>
               </Box>
               <Button
                 variant="contained"
                 fullWidth
-                className={styles['login__container__form-area__form__button']}
-<<<<<<< HEAD
-=======
+                className={styles["login__container__form-area__form__button"]}
                 sx={{
-                  backgroundColor: colors.bluePrimary500, textTransform: "none"
+                  backgroundColor: colors.bluePrimary500,
+                  textTransform: "none",
                 }}
->>>>>>> fa0f9d98927fa9d1bef3df5e214280aeca74b432
               >
                 Entrar
               </Button>
-              <Box className={styles['login__container__form-area__form__redirect']}>
-                <p className={styles['login__container__form-area__form__redirect__text']}>
+              <Box
+                className={
+                  styles["login__container__form-area__form__redirect"]
+                }
+              >
+                <p
+                  className={
+                    styles["login__container__form-area__form__redirect__text"]
+                  }
+                >
                   Ainda não tem uma conta?{" "}
                   <Link
-                    className={styles['login__container__form-area__form__redirect__text__link']}
+                    className={
+                      styles[
+                        "login__container__form-area__form__redirect__text__link"
+                      ]
+                    }
                     href="http://localhost:3000/cadastro"
                   >
                     Cadastre-se
@@ -146,20 +145,19 @@ export default function Login() {
           </Box>
         </Box>
 
-        <Box className={styles['login__container-side']}>
+        <Box className={styles["login__container-side"]}>
           <Image
             src="/undraw_transfer-money.svg"
             width={467}
             height={448}
             alt="Bem Vindo"
             className="login__container-side__image"
-<<<<<<< HEAD
-=======
             style={{ width: "40%", height: "auto" }}
->>>>>>> fa0f9d98927fa9d1bef3df5e214280aeca74b432
           />
-          <h2 className={styles['login__container-side__title']}>Seja bem-vindo!</h2>
-          <h3 className={styles['login__container-side__description']}>
+          <h2 className={styles["login__container-side__title"]}>
+            Seja bem-vindo!
+          </h2>
+          <h3 className={styles["login__container-side__description"]}>
             Descubra soluções financeiras inovadoras e confiáveis para garantir
             um futuro próspero
           </h3>
